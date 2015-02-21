74add01565df81f8baf72de23792294afc8d0585
jsajnani79 <jsajnani79@gmail.com>
2015-02-21 14:34:56 -0800

    ultrasound code added

--- bigballer/bigballer.ino
+++ bigballer/bigballer.ino
@@ -1,4 +1,5 @@
 #include "Bot.h"
+#include "NewPing.h"
 
 /**
  * MAIN INO FOR ROBOT.
@@ -11,12 +12,20 @@
 
 /*---------------- Module Defines ---------------------------*/
 
+//MOTORS
 #define ENABLE_PIN_LEFT 4
 #define DIR_PIN_LEFT 5
 #define ENABLE_PIN_RIGHT 2
 #define DIR_PIN_RIGHT 3
 
+//ULTRASOUND
+#define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN     13  // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define MAX_DISTANCE 30 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+
+
 Bot* robot;
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
 /*---------------- Module Function Prototypes ---------------*/
 
@@ -33,6 +42,13 @@ void loop() {
     robot->stop();
     Serial.println("stopping");
   }
+  
+  //ULTRASOUND
+  delay(50);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  Serial.print("Ping: ");
+  Serial.print(uS / US_ROUNDTRIP_CM); // Convert ping time to distance in cm and print result (0 = outside set distance range)
+  Serial.println("cm");
   
 //  Serial.println("100 forward");
 //  robot->moveForward(100);
